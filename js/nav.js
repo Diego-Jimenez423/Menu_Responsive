f501cf9d245245ce6496f7ba846f98b49d61bf6a
Diego Jimenez <66481204+Diego-Jimenez423@users.noreply.github.com>
2020-08-25 11:59:10 -0600

    Se mejoro el menú

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+var botonlis4 = document.getElementById("lisBoton4");
+var boton4 = document.getElementById('boton4');
+
+function desplegarBoton4(){
+    botonlis4.classList.add('mostrar');
+};
+
+boton4.addEventListener("click",desplegarBoton4,true);
